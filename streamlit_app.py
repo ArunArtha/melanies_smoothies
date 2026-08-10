2a261ee5f44445a769d24c8a6954bca1bb74aff6
ArunArtha <arunkumar.murugesh@thinkartha.com>
2026-08-10 17:49:24 +0530

    Replace text display with dataframe for smoothie data

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ if Ingredients_List:
 
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
